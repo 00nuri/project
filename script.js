92c200e2f0133a11f1539031f4b884b7ef64b58a
00nuri <85096041+00nuri@users.noreply.github.com>
2022-06-05 01:21:33 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+var req =new XMLHttpRequest();
+req.open("GET","./json/image_list.json");
+req.onreadystatechange = function(){
+	if( this.readyState == 4){
+		//console.log(this.response);
+	var date = JSON.parse(this.response);
+		for(var i = 0; i< date.length ; i++){
+		var div	= document.createElement("div");
+			div.setAttribute("class","image");
+			var img = document.createElement("img");
+			img.src = date[i];
+			div.appendChild(img);
+			document.body.appendChild(div);
+			
+		}
+	}
+}
+req.send();
